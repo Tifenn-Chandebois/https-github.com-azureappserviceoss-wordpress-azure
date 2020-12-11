8560fd7a723106f217a5b9aa664bc5801b944671
Tifenn-Chandebois <73821335+Tifenn-Chandebois@users.noreply.github.com>
2020-12-11 15:53:47 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import page from './page.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={page} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
